5c03efe19daf271789fd3e0c78d854a611354a12
besito <bessam.alqaisi@gmail.com>
2024-02-09 13:17:40 +0100

    added factory and total passive cookies

--- click.py
+++ click.py
@@ -8,8 +8,8 @@ pygame.init()
 
 # battlepass
 # more upgrades
-# show passive cookies
 # saving system
+# random events
 
 
 # Set up the drawing window
@@ -17,18 +17,19 @@ screen = pygame.display.set_mode([500, 500])
 
 
 # money
-cookies = 0
+cookies = 200
 
 
 # upgrades
 cookie_click = 1
 babushkas = 0
-passive_cookie = 0
+factory = 0
 
 
 # upgrade costs
 upgradecost = 20
-babushka_cost = 100
+babushka_cost = 60
+factory_cost = 100
 
 
 
@@ -37,17 +38,25 @@ upgradeSurface = pygame.Surface ((100, 50))
 upgradeRect = pygame.Rect(20, 50, 100, 100)
 font = pygame.font.Font(None, 30)
 
-# text_render(250, 120, f"babushkas: {babushkas}")
+
 babushkaSurface = pygame.Surface ((100, 50))
 babushkaRect = pygame.Rect(250, 50, 100, 100)
 
+factorySurface = pygame.Surface ((100, 50))
+factoryRect = pygame.Rect(20, 180, 100, 100)
 
 
-# passive cookie
+
+# passive babushka
 BABUSKA_MAKE_COOKIE_EVENT_TYPE = pygame.USEREVENT + 1
 
 pygame.time.set_timer(pygame.event.Event(BABUSKA_MAKE_COOKIE_EVENT_TYPE), 1000)
 
+
+# passive factory
+PASSIVE_FACTORY = pygame.USEREVENT + 2
+
+pygame.time.set_timer(pygame.event.Event(PASSIVE_FACTORY), 1000)
 
 
 # transaction shop function
@@ -81,9 +90,15 @@ while running:
         if event.type == BABUSKA_MAKE_COOKIE_EVENT_TYPE:
             cookies += babushkas
 
-        # COOKIE TRANSACTION
+        # passive factory addition
+        if event.type == PASSIVE_FACTORY:
+            cookies += factory*5
+
+            
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
+
+            # COOKIE TRANSACTION
             if upgradeRect.collidepoint(pos):
                 upgradecost, cookie_click = transaction(upgradecost, cookie_click, 1.4)
                 
@@ -92,17 +107,23 @@ while running:
             if babushkaRect.collidepoint(pos):
                 babushka_cost, babushkas = transaction(babushka_cost, babushkas, 1.5)
 
+            # FACTORY TRANSACTION
+            if factoryRect.collidepoint(pos):
+                factory_cost, factory = transaction(factory_cost, factory, 1.6)
+
 
         # listen for keyboard events
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 cookies += cookie_click
-       
+
+
 
 
     # Fill the background with white
     screen.fill((255, 255, 255))
 
+    text_render(250, 20, f"cookies / second: {babushkas+factory*5}")
 
     # display cookies
     text_render(20, 20, f"cookies: {cookies}")
@@ -117,7 +138,13 @@ while running:
     text_render(250, 120, f"babushkas: {babushkas}")
 
     #display babushka cost
-    text_render(250, 140, f"cost of babushka: {babushka_cost}")
+    text_render(250, 140, f"babushka cost: {babushka_cost}")
+
+    # display factory
+    text_render(20, 250, f"factories: {factory}")
+
+    #display factory cost
+    text_render(20, 270, f"factory cost: {factory_cost}")
     
         
 
@@ -125,9 +152,14 @@ while running:
     upgradeSurface.fill ((0, 0, 0))
     screen.blit (upgradeSurface, upgradeRect)
 
-      # babushka Button
+    # babushka Button
     babushkaSurface.fill ((235, 150, 70))
     screen.blit (babushkaSurface, babushkaRect)
+
+    # factory button
+    factorySurface.fill((158, 158, 158))
+    screen.blit(factorySurface, factoryRect)
+    
    
 
 
